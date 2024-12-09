817c5b022ace4e4ab9d5edcec47784394fa76e18
XZDebug <debug.world@outlook.com>
2024-12-10 01:56:59 +0800

    first commit

---
+++ docs/.vuepress/.temp/internal/clientConfigs.js
@@ -0,0 +1,23 @@
+import * as clientConfig0 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-active-header-links@2.0.0-rc.63_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@_7g7z5mvgwq3x6oxqwiclnbdhga/node_modules/@vuepress/plugin-active-header-links/lib/client/config.js'
+import * as clientConfig1 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-back-to-top@2.0.0-rc.65_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_xyr5mk434bzy5uve5j5hpwillu/node_modules/@vuepress/plugin-back-to-top/lib/client/config.js'
+import * as clientConfig2 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-copy-code@2.0.0-rc.65_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.1_h7xdwjr52xryuvdkrtpboq6e7a/node_modules/@vuepress/plugin-copy-code/lib/client/config.js'
+import * as clientConfig3 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-markdown-hint@2.0.0-rc.65_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+_ssunrjy53xsdlvcxyt4yhvmow4/node_modules/@vuepress/plugin-markdown-hint/lib/client/config.js'
+import * as clientConfig4 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-medium-zoom@2.0.0-rc.65_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc_f5ehyxru7ktitbocsnthexfd6q/node_modules/@vuepress/plugin-medium-zoom/lib/client/config.js'
+import * as clientConfig5 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-nprogress@2.0.0-rc.65_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc.1_l6asfnfn7rdh2djouk6r57stdy/node_modules/@vuepress/plugin-nprogress/lib/client/config.js'
+import * as clientConfig6 from 'D:/Code/GUGUbot/GUGUbot-Docs/docs/.vuepress/.temp/prismjs/config.js'
+import * as clientConfig7 from 'D:/Code/GUGUbot/GUGUbot-Docs/docs/.vuepress/.temp/markdown-tab/config.js'
+import * as clientConfig8 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-theme-data@2.0.0-rc.64_vuepress@2.0.0-rc.18_@vuepress+bundler-vite@2.0.0-rc._ovugwkvwxwieh744oj2jvxjxdq/node_modules/@vuepress/plugin-theme-data/lib/client/config.js'
+import * as clientConfig9 from 'D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+theme-default@2.0.0-rc.65_markdown-it@14.1.0_sass@1.82.0_vuepress@2.0.0-rc.18_@vuep_gt3kq74ussjfm5idcmf2qxc3wi/node_modules/@vuepress/theme-default/lib/client/config.js'
+
+export const clientConfigs = [
+  clientConfig0,
+  clientConfig1,
+  clientConfig2,
+  clientConfig3,
+  clientConfig4,
+  clientConfig5,
+  clientConfig6,
+  clientConfig7,
+  clientConfig8,
+  clientConfig9,
+].map((m) => m.default).filter(Boolean)
---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,12 @@
+import "D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.62_@vueuse+core@12.0.0_vuepress@2.0.0-rc.18_@vuepress+b_vlhxshp3nemsilszh26mq7tqxm/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+plugin-prismjs@2.0.0-rc.65_@vueuse+core@12.0.0_vuepress@2.0.0-rc.18_@vuepress+bundl_jionrk7ctlpvdekqoslyy55ble/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.62_@vueuse+core@12.0.0_vuepress@2.0.0-rc.18_@vuepress+b_vlhxshp3nemsilszh26mq7tqxm/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.62_@vueuse+core@12.0.0_vuepress@2.0.0-rc.18_@vuepress+b_vlhxshp3nemsilszh26mq7tqxm/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.62_@vueuse+core@12.0.0_vuepress@2.0.0-rc.18_@vuepress+b_vlhxshp3nemsilszh26mq7tqxm/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "D:/Code/GUGUbot/GUGUbot-Docs/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.62_@vueuse+core@12.0.0_vuepress@2.0.0-rc.18_@vuepress+b_vlhxshp3nemsilszh26mq7tqxm/node_modules/@vuepress/highlighter-helper/lib/client/index.js"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
